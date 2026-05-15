8a9780b6a82b5ec017ee7491b5a2dd48ce6f9e47
nicolasshul <74377515+nicolasshul@users.noreply.github.com>
2026-05-15 14:14:39 +0200

    changed nvim theme

---
+++ mac/nvim/lua/plugins/themes/nightfall.lua
@@ -0,0 +1,10 @@
+return {
+  "2giosangmitom/nightfall.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {}, -- Add custom configuration here
+  config = function(_, opts)
+    require("nightfall").setup(opts)
+    vim.cmd("colorscheme maron") -- Choose from: nightfall, deeper-night, maron, nord
+  end,
+}
